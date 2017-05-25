e12fff12874db933ccaa00e2cb498873120d4dbb
Danny Berger <dpb587@gmail.com>
2017-05-24 22:26:46 -0700

    containerize

--- ci/tasks/update/execute.sh
+++ ci/tasks/update/execute.sh
@@ -4,16 +4,16 @@ set -eu -o pipefail
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../.."
 
+export GIT_COMMITTER_NAME=Concourse
+export GIT_COMMITTER_EMAIL=concourse@localhost
+export GIT_AUTHOR_NAME="${GIT_AUTHOR_NAME:-$GIT_COMMITTER_NAME}"
+export GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL:-$GIT_COMMITTER_EMAIL}"
+
 git clone --quiet file://$PWD/repo repo-output
 
 cd repo-output
 
 ./bin/all
-
-export GIT_COMMITTER_NAME=Concourse
-export GIT_COMMITTER_EMAIL=concourse@localhost
-export GIT_AUTHOR_NAME="${GIT_AUTHOR_NAME:-$GIT_COMMITTER_NAME}"
-export GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL:-$GIT_COMMITTER_EMAIL}"
 
 git add .
 git commit -m "Update repositories" || true
