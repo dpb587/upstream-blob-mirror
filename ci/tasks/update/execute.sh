35c50fae7bb5ba371fac64573e9cef7a55be5626
Danny Berger <dpb587@gmail.com>
2017-06-11 14:45:23 -0700

    remote uri

--- ci/tasks/update/execute.sh
+++ ci/tasks/update/execute.sh
@@ -11,6 +11,8 @@ export GIT_AUTHOR_EMAIL="${GIT_AUTHOR_EMAIL:-$GIT_COMMITTER_EMAIL}"
 
 git clone --quiet file://$PWD/repo repo-output
 
+export GIT_REMOTE_URI=$( cd repo ; git remote get-url origin )
+
 cd repo-output
 
 ./bin/all
